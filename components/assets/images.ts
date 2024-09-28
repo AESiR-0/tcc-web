01fffdedb879428f400a3de198f89245f6565593
AESiR <cyborgkiller1008@gmail.com>
2024-09-28 22:28:25 +0530

    Refactor image imports in images.ts

--- components/assets/images.ts
+++ components/assets/images.ts
@@ -1,39 +1,36 @@
 // import React from 'react'
-import factoryPov from "@/public/static/factory/VBV01082.jpg"
-import  factoryOut from "@/public/static/factory/VBV01110.jpg"
-import  factoryCenter from "@/public/static/factory/VBV01118.jpg"
-import  wireBetweenmachines from "@/public/static/factory/VBV01136.jpg"
-import  workerWorkingWide from "@/public/static/factory/VBV01148.jpg"
-import  workerWorkingNormal from "@/public/static/factory/VBV01163.jpg"
-import  wirecClose1 from "@/public/static/factory/VBV01165.jpg"
-import  wirecClose2 from "@/public/static/factory/VBV01166.jpg"
-import  wireWheelWide from "@/public/static/factory/VBV01201.jpg"
-import  wireWheelNormal from "@/public/static/factory/VBV01204.jpg"
-import  blurWorkerLookingAway from "@/public/static/factory/VBV01214.jpg"
-import  workerSafetyHat from "@/public/static/factory/VBV01215.jpg"
-import  workerWorkingBetweenWires from "@/public/static/factory/VBV01233.jpg"
-import  workerClapping from "@/public/static/factory/VBV01305.jpg"
-import  wireTopViewDark from "@/public/static/factory/VBV01326.jpg"
-import  wireTopViewLight from "@/public/static/factory/VBV01339.jpg"
-  
+import factoryPov from "/static/factory/VBV01082.jpg";
+import factoryOut from "/static/factory/VBV01110.jpg";
+import factoryCenter from "/static/factory/VBV01118.jpg";
+import wireBetweenmachines from "/static/factory/VBV01136.jpg";
+import workerWorkingWide from "/static/factory/VBV01148.jpg";
+import workerWorkingNormal from "/static/factory/VBV01163.jpg";
+import wirecClose1 from "/static/factory/VBV01165.jpg";
+import wirecClose2 from "/static/factory/VBV01166.jpg";
+import wireWheelWide from "/static/factory/VBV01201.jpg";
+import wireWheelNormal from "/static/factory/VBV01204.jpg";
+import blurWorkerLookingAway from "/static/factory/VBV01214.jpg";
+import workerSafetyHat from "/static/factory/VBV01215.jpg";
+import workerWorkingBetweenWires from "/static/factory/VBV01233.jpg";
+import workerClapping from "/static/factory/VBV01305.jpg";
+import wireTopViewDark from "/static/factory/VBV01326.jpg";
+import wireTopViewLight from "/static/factory/VBV01339.jpg";
 
 export const images = {
-    
-        factoryPov,
-        factoryOut,
-        factoryCenter,
-        wireBetweenmachines,
-        workerWorkingWide,
-        workerWorkingNormal,
-        wirecClose1,
-        wirecClose2,
-        wireWheelWide,
-        wireWheelNormal,
-        blurWorkerLookingAway,
-        workerSafetyHat,
-        workerWorkingBetweenWires,
-        workerClapping,
-        wireTopViewDark,
-        wireTopViewLight,
-      
-}
+  factoryPov,
+  factoryOut,
+  factoryCenter,
+  wireBetweenmachines,
+  workerWorkingWide,
+  workerWorkingNormal,
+  wirecClose1,
+  wirecClose2,
+  wireWheelWide,
+  wireWheelNormal,
+  blurWorkerLookingAway,
+  workerSafetyHat,
+  workerWorkingBetweenWires,
+  workerClapping,
+  wireTopViewDark,
+  wireTopViewLight,
+};
